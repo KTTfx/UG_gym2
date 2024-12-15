b5c047a4653bd52ca67755a60675f069a821c88e
kev-69 <bismarkobuobi19@gmail.com>
2024-12-15 10:33:09 +0000

    handled logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import logo from '../ug_gym_logo2.png';
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
-  const isLoggedIn = location.pathname.includes('/dashboard') || location.pathname.includes('/university-dashboard');
+  const isLoggedIn = location.pathname.includes('/dashboard') || location.pathname.includes('/university-dashboard') || location.pathname.includes('/payment-pending');
 
   const handleLogout = () => {
     navigate('/');
